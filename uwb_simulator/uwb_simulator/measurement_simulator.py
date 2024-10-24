9d32979b8af5b26320c66e00a84afb3a7c303326
amartinezsilva <andresmarsil77@gmail.com>
2024-10-24 16:42:32 +0200

    measurement simulator working

--- uwb_simulator/uwb_simulator/measurement_simulator.py
+++ uwb_simulator/uwb_simulator/measurement_simulator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rclpy
+import rclpy.duration
 from rclpy.node import Node
 import numpy as np
 import random
@@ -20,58 +21,105 @@ class MeasurementSimulator(Node):
         super().__init__('measurement_simulator')
         self.get_logger().info("Measurement Simulator Node Started")
 
-        # Declare and acquire `target_frame` parameter
-        self.target_frame = self.declare_parameter(
-          'target_frame', 'turtle1').get_parameter_value().string_value
+        self.rate = 10.0
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        # Create measurement publisher tag2
+        # Create measurement publisher tag1
         self.t1a1_publisher = self.create_publisher(Float32, 'range/t1a1', 1)
         self.t1a2_publisher = self.create_publisher(Float32, 'range/t1a2', 1)
         self.t1a3_publisher = self.create_publisher(Float32, 'range/t1a3', 1)
         self.t1a4_publisher = self.create_publisher(Float32, 'range/t1a4', 1)
-        # Create measurement publisher
+        # Create measurement publisher tag2
         self.t2a1_publisher = self.create_publisher(Float32, 'range/t2a1', 1)
         self.t2a2_publisher = self.create_publisher(Float32, 'range/t2a2', 1)
         self.t2a3_publisher = self.create_publisher(Float32, 'range/t2a3', 1)
         self.t2a4_publisher = self.create_publisher(Float32, 'range/t2a4', 1)
 
+        self.distance_publisher = self.create_publisher(Float32, 'range/uav_to_groundvehicle', 1)
 
-        # Call on_timer function every second
-        self.timer1 = self.create_timer(1.0, self.on_timer('t1','a1', self.t1a1_publisher))
-        self.timer2 = self.create_timer(1.0, self.on_timer('t1','a2', self.t1a2_publisher))
-        self.timer3 = self.create_timer(1.0, self.on_timer('t1','a3', self.t1a3_publisher))
-        self.timer4 = self.create_timer(1.0, self.on_timer('t1','a4', self.t1a4_publisher))
+        self.timer1 = self.create_timer(1./self.rate, self.on_timer_t1)
+        self.timer2 = self.create_timer(1./self.rate, self.on_timer_t2)
 
-        # # Call on_timer function every second
-        self.timer5 = self.create_timer(1.0, self.on_timer('t2','a1', self.t2a1_publisher))
-        self.timer6 = self.create_timer(1.0, self.on_timer('t2','a2', self.t2a2_publisher))
-        self.timer7 = self.create_timer(1.0, self.on_timer('t2','a3', self.t2a3_publisher))
-        self.timer8 = self.create_timer(1.0, self.on_timer('t2','a4', self.t2a4_publisher))
+        #self.timer = self.create_timer(1./self.rate, self.on_timer_debug)
+        #self.timer = self.create_timer(1./self.rate, self.on_timer('uav', 'ground_vehicle', self.distance_publisher))
 
-        #self.timer = self.create_timer(1.0, self.on_timer('uav','ground_vehicle', self.distance_publisher))
-
-
-    def on_timer(self, to_frame_rel, from_frame_rel, publisher):
+    def on_timer_debug(self):
 
         try:
             t = self.tf_buffer.lookup_transform(
-                to_frame_rel,
-                from_frame_rel,
-                rclpy.time.Time())
+                "uav",
+                "ground_vehicle",
+                rclpy.time.Time(), rclpy.duration.Duration(seconds=2.0))
+            
+            distance = Float32()
+            distance.data = np.sqrt(t.transform.translation.x**2 + t.transform.translation.y**2 + t.transform.translation.z**2) + np.random.normal(0, 0.2)
+            self.get_logger().info("Distance: %f" % distance.data)
+            
         except TransformException as ex:
             self.get_logger().info(
-                f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
+                f'Could not transform "ground vehicle" to "uav": {ex}')
             return
         
-        distance = Float32()
+        self.distance_publisher.publish(distance)
+    
+    #Publish simulated measures from tag1 to anchors using simulated ground truth
 
-        distance.data = np.sqrt(t.transform.translation.x**2 + t.transform.translation.y**2 + t.transform.translation.z**2) + np.random.normal(0, 0.2)
+    def on_timer_t1(self):
+   
+        anchors_publishers =	{
+        'a1': self.t1a1_publisher,
+        'a2': self.t1a2_publisher,
+        'a3': self.t1a3_publisher,
+        'a4': self.t1a4_publisher
+        } 
+
+        for anchor, publisher in anchors_publishers.items(): 
+            try:
+                t = self.tf_buffer.lookup_transform(
+                    't1',
+                    anchor,
+                    rclpy.time.Time())
+                
+                distance = Float32()
+                distance.data = np.sqrt(t.transform.translation.x**2 + t.transform.translation.y**2 + t.transform.translation.z**2) + np.random.normal(0, 0.2)
+                
+            except TransformException as ex:
+                self.get_logger().info(
+                    f'Could not transform t1 to {anchor}: {ex}')
+                return
         
-        publisher.publish(distance)
+            publisher.publish(distance)
 
+
+    #Publish simulated measures from tag2 to anchors
+
+    def on_timer_t2(self):
+   
+        anchors_publishers =	{
+        'a1': self.t2a1_publisher,
+        'a2': self.t2a2_publisher,
+        'a3': self.t2a3_publisher,
+        'a4': self.t2a4_publisher
+        } 
+
+        for anchor, publisher in anchors_publishers.items(): 
+            try:
+                t = self.tf_buffer.lookup_transform(
+                    't2',
+                    anchor,
+                    rclpy.time.Time())
+                
+                distance = Float32()
+                distance.data = np.sqrt(t.transform.translation.x**2 + t.transform.translation.y**2 + t.transform.translation.z**2) + np.random.normal(0, 0.2)
+                
+            except TransformException as ex:
+                self.get_logger().info(
+                    f'Could not transform t2 to {anchor}: {ex}')
+                return
+        
+            publisher.publish(distance)
 
 
 
