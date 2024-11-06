2bdd8cfa5ed2e1c5dd25ac167d7384f043858366
amartinezsilva <andresmarsil77@gmail.com>
2024-11-06 16:33:47 +0100

    optimize on timer w/ latest data from both tags

--- uwb_localization/src/optimizer_node_eliko.cpp
+++ uwb_localization/src/optimizer_node_eliko.cpp
@@ -20,6 +20,7 @@
 #include <Eigen/Dense>
 #include <utility>
 #include <unordered_map>
+#include <chrono>
 
 using MeasurementKey = std::pair<std::string, std::string>; // (anchor_sn, tag_sn)
 
@@ -48,8 +49,8 @@ public:
     // eliko_tags_coords_sub_ = this->create_subscription<eliko_messages::msg::TagCoordsList>(
     //             "/eliko/TagCoords", 10, std::bind(&ElikoOptimizationNode::tags_coords_cb_, this, std::placeholders::_1));
 
-    eliko_anchors_coords_sub_ = this->create_subscription<eliko_messages::msg::AnchorCoordsList>(
-                "/eliko/AnchorCoords", 10, std::bind(&ElikoOptimizationNode::anchors_coords_cb_, this, std::placeholders::_1));
+    // eliko_anchors_coords_sub_ = this->create_subscription<eliko_messages::msg::AnchorCoordsList>(
+    //             "/eliko/AnchorCoords", 10, std::bind(&ElikoOptimizationNode::anchors_coords_cb_, this, std::placeholders::_1));
     
     dji_attitude_sub_ = this->create_subscription<geometry_msgs::msg::QuaternionStamped>(
                 "/dji_sdk/attitude", 10, std::bind(&ElikoOptimizationNode::attitude_cb_, this, std::placeholders::_1));
@@ -58,13 +59,19 @@ public:
     tf_publisher_ = this->create_publisher<geometry_msgs::msg::TransformStamped>("eliko_optimization_node/optimized_T", 10);
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
+    optimizer_time_window_s_ = 0.2; //use only messages in this window for optimization
+    min_measurements_ = 3; //min number of measurements for running optimizer
+
+    optimization_timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(int(optimizer_time_window_s_*1000)), std::bind(&ElikoOptimizationNode::optimizer_cb_, this));
+
     anchor_positions_ = {
-        {"0x0009D6", {0.28, -0.24, 0.0}}, {"0x0009ES", {0.28, 0.24, 0.0}},
-        {"0x0016FA", {-0.28, 0.24, 0.0}}, {"0x0016CF", {-0.28, 0.24, 0.0}}
+        {"0x0009D6", {-0.32, 0.3, 0.875}}, {"0x0009E5", {0.32, -0.3, 0.875}},
+        {"0x0016FA", {0.32, 0.3, 0.33}}, {"0x0016CF", {-0.32, -0.3, 0.33}}
     };
     
     tag_positions_ = {
-        {"0x001155", {0.5, 0.5, 0.25}}, {"0x001397", {-0.5, -0.5, 0.25}}
+        {"0x001155", {-0.24, -0.24, -0.06}}, {"0x001397", {0.24, 0.24, -0.06}}
     };
 
     eliko_frame_id_ = "arco/eliko"; //frame of the eliko system-> same as robot frame
@@ -75,62 +82,96 @@ public:
     pitch_ = 0.0;  // Example pitch in radians
     yaw_ = 0.0;    // Initial guess for yaw
 
-    t_ = {0.0, 0.0, -1.0};       // Translation That_ts (x, y, z)
+    t_ = {0.0, 0.0, 0.0};       // Translation That_ts (x, y, z)
 
-    window_size_ = 5; //moving average sample window (N)
+    moving_average_window_size_ = 5; //moving average sample window (N)
 
     RCLCPP_INFO(this->get_logger(), "Eliko Optimization Node initialized.");
   }
 
 private:
 
-  // Callback for each measurement
-  void distances_coords_cb_(const eliko_messages::msg::DistancesList::SharedPtr msg) {
-      // Clear old measurements each cycle
-        current_measurements_.clear();
-        
-        RCLCPP_INFO(this->get_logger(), "[Eliko optimizer node] Got %ld measurements", msg->anchor_distances.size());
+    // Callback for each measurement
+    void distances_coords_cb_(const eliko_messages::msg::DistancesList::SharedPtr msg) {
+            
+            if(msg->anchor_distances[0].tag_sn == "0x001155") distances_t1_ = *msg;
+            else if(msg->anchor_distances[0].tag_sn == "0x001397") distances_t2_ = *msg;
 
-        for (const auto& distance_msg : msg->anchor_distances) {
-            // Store distance measurements using (anchor_sn, tag_sn) as key
-            MeasurementKey key(distance_msg.anchor_sn, distance_msg.tag_sn);
-            current_measurements_[key] = distance_msg.distance / 100.0; // Convert to meters
-            RCLCPP_INFO(this->get_logger(), "[Eliko optimizer node] Distance received from Anchor %s to tag %s: %.2f cm", distance_msg.anchor_sn.c_str(), distance_msg.tag_sn.c_str(), distance_msg.distance);
-        }
+            for (const auto& distance_msg : msg->anchor_distances) {
+                RCLCPP_INFO(this->get_logger(), "[Eliko optimizer node] Distance received from Anchor %s to tag %s: %.2f cm", distance_msg.anchor_sn.c_str(), distance_msg.tag_sn.c_str(), distance_msg.distance);
+            }
 
-        run_optimization();
-  }
-
-
-  void tags_coords_cb_(const eliko_messages::msg::TagCoordsList::SharedPtr msg) {
-        // Update tag coordinates based on received data
-        for (const auto& tag_coord : msg->tag_coords) {
-            Eigen::Vector3d position(tag_coord.x_coord, tag_coord.y_coord, tag_coord.z_coord);
-            tag_positions_[tag_coord.tag_sn] = position;
-
-            RCLCPP_DEBUG(this->get_logger(), "[Eliko optimizer node] Tag %s coordinates: [%.2f, %.2f, %.2f]", tag_coord.tag_sn.c_str(), position[0], position[1], position[2]);
-        }
-  }
-
-   void anchors_coords_cb_(const eliko_messages::msg::AnchorCoordsList::SharedPtr msg) {
-     // Update anchor coordinates based on received data
-        for (const auto& anchor_coord : msg->anchor_coords) {
-            Eigen::Vector3d position(anchor_coord.x_coord, anchor_coord.y_coord, anchor_coord.z_coord);
-            anchor_positions_[anchor_coord.anchor_sn] = position;
-
-            RCLCPP_DEBUG(this->get_logger(), "[Eliko optimizer node] Anchor %s coordinates: [%.2f, %.2f, %.2f]", anchor_coord.anchor_sn.c_str(), position[0], position[1], position[2]);
-        }
-  }
-
-  void attitude_cb_(const geometry_msgs::msg::QuaternionStamped::SharedPtr msg) {
-
-        const auto& q = msg->quaternion;
-        // Convert the quaternion to roll, pitch, yaw
-        tf2::Quaternion tf_q(q.x, q.y, q.z, q.w);
-        tf2::Matrix3x3 m(tf_q);
-        double yaw;
-        m.getRPY(roll_, pitch_, yaw);  // Get roll and pitch, but ignore yaw as we optimize it separately
     }
+
+    void optimizer_cb_() {
+
+        rclcpp::Time current_time = this->get_clock()->now();
+
+        if(!distances_t1_.anchor_distances.empty() && current_time - distances_t1_.header.stamp < rclcpp::Duration::from_seconds(optimizer_time_window_s_)){
+            for (const auto& distance_msg : distances_t1_.anchor_distances) {
+                    // Store distance measurements using (anchor_sn, tag_sn) as key
+                    MeasurementKey key(distance_msg.anchor_sn, distance_msg.tag_sn);
+                    current_measurements_[key] = distance_msg.distance / 100.0; // Convert to meters
+                }
+            RCLCPP_DEBUG(this->get_logger(), "[Eliko optimizer node] Using %ld measurements from t1", distances_t1_.anchor_distances.size());
+        }
+
+        if(!distances_t2_.anchor_distances.empty() && current_time - distances_t2_.header.stamp < rclcpp::Duration::from_seconds(optimizer_time_window_s_)){
+            for (const auto& distance_msg : distances_t2_.anchor_distances) {
+                    // Store distance measurements using (anchor_sn, tag_sn) as key
+                    MeasurementKey key(distance_msg.anchor_sn, distance_msg.tag_sn);
+                    current_measurements_[key] = distance_msg.distance / 100.0; // Convert to meters
+                }
+            RCLCPP_DEBUG(this->get_logger(), "[Eliko optimizer node] Using %ld measurements from t2", distances_t2_.anchor_distances.size());
+        }
+ 
+        //Ensure at least one measurement from each of the tags
+        if(current_measurements_.size() >= min_measurements_){
+            RCLCPP_INFO(this->get_logger(), "[Eliko optimizer node] Running optimizer with %ld measurements", current_measurements_.size());
+            run_optimization();
+        }
+        else{
+            RCLCPP_WARN(this->get_logger(), "[Eliko optimizer node] Not enough data to run optimization");
+        }
+
+        current_measurements_.clear();
+        distances_t1_.anchor_distances.clear();
+        distances_t2_.anchor_distances.clear();
+
+    }
+
+
+
+//   void tags_coords_cb_(const eliko_messages::msg::TagCoordsList::SharedPtr msg) {
+//         // Update tag coordinates based on received data
+//         for (const auto& tag_coord : msg->tag_coords) {
+//             Eigen::Vector3d position(tag_coord.x_coord, tag_coord.y_coord, tag_coord.z_coord);
+//             tag_positions_[tag_coord.tag_sn] = position;
+
+//             RCLCPP_DEBUG(this->get_logger(), "[Eliko optimizer node] Tag %s coordinates: [%.2f, %.2f, %.2f]", tag_coord.tag_sn.c_str(), position[0], position[1], position[2]);
+//         }
+//   }
+
+//    void anchors_coords_cb_(const eliko_messages::msg::AnchorCoordsList::SharedPtr msg) {
+//      // Update anchor coordinates based on received data
+//         for (const auto& anchor_coord : msg->anchor_coords) {
+//             Eigen::Vector3d position(anchor_coord.x_coord, anchor_coord.y_coord, anchor_coord.z_coord);
+//             anchor_positions_[anchor_coord.anchor_sn] = position;
+
+//             RCLCPP_DEBUG(this->get_logger(), "[Eliko optimizer node] Anchor %s coordinates: [%.2f, %.2f, %.2f]", anchor_coord.anchor_sn.c_str(), position[0], position[1], position[2]);
+//         }
+//   }
+
+    void attitude_cb_(const geometry_msgs::msg::QuaternionStamped::SharedPtr msg) {
+
+            const auto& q = msg->quaternion;
+            // Convert the quaternion to roll, pitch, yaw
+            tf2::Quaternion tf_q(q.x, q.y, q.z, q.w);
+            tf2::Matrix3x3 m(tf_q);
+            double yaw;
+            m.getRPY(roll_, pitch_, yaw);  // Get roll and pitch, but ignore yaw as we optimize it separately
+        }
+
 
   std::pair<std::array<double, 3>, double> moving_average(const double yaw, const std::array<double, 3>& t) {
     
@@ -139,7 +180,7 @@ private:
         yaw_history_.push_back(yaw);
 
         // Remove oldest if history exceeds desired size
-        if (translation_history_.size() > window_size_) {
+        if (translation_history_.size() > moving_average_window_size_) {
             translation_history_.pop_front();
             yaw_history_.pop_front();
         }
@@ -233,6 +274,7 @@ private:
 
     // Run the optimization once all measurements are received
     void run_optimization() {
+
             ceres::Problem problem;
 
             // Dynamically add residuals for available measurements
@@ -273,11 +315,6 @@ private:
 
                 // Construct transformation matrix T with optimized yaw, roll, pitch, and translation
                 Eigen::Matrix4d That_ts = build_transformation_matrix(roll_, pitch_, yaw_, t_);
-
-                // Convert Eigen::Matrix4d to a string
-                std::stringstream ss;
-                ss << That_ts;
-                RCLCPP_INFO(this->get_logger(), "Optimized Transformation Matrix:\n%s", ss.str().c_str());
 
                 publish_transform(That_ts);
             }
@@ -325,14 +362,12 @@ private:
 
     
     rclcpp::Subscription<eliko_messages::msg::DistancesList>::SharedPtr eliko_distances_sub_;
-    rclcpp::Subscription<eliko_messages::msg::AnchorCoordsList>::SharedPtr eliko_anchors_coords_sub_;
-    rclcpp::Subscription<eliko_messages::msg::TagCoordsList>::SharedPtr eliko_tags_coords_sub_;
+    // rclcpp::Subscription<eliko_messages::msg::AnchorCoordsList>::SharedPtr eliko_anchors_coords_sub_;
+    // rclcpp::Subscription<eliko_messages::msg::TagCoordsList>::SharedPtr eliko_tags_coords_sub_;
     rclcpp::Subscription<geometry_msgs::msg::QuaternionStamped>::SharedPtr dji_attitude_sub_;
+    rclcpp::TimerBase::SharedPtr optimization_timer_;
 
-    eliko_messages::msg::DistancesList all_distances_;
-    eliko_messages::msg::AnchorCoordsList anchors_coords_;
-    eliko_messages::msg::TagCoordsList tags_coords_;
-
+    eliko_messages::msg::DistancesList distances_t1_, distances_t2_;
     std::unordered_map<MeasurementKey, float, PairHash> current_measurements_;
 
     rclcpp::Publisher<geometry_msgs::msg::TransformStamped>::SharedPtr tf_publisher_;
@@ -353,12 +388,15 @@ private:
 
     std::deque<std::array<double, 3>> translation_history_;
     std::deque<double> yaw_history_;
-    size_t window_size_;
+    size_t moving_average_window_size_;
+    size_t min_measurements_;
+    double optimizer_time_window_s_;
 
 };
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<ElikoOptimizationNode>();
+    node->set_parameter(rclcpp::Parameter("use_sim_time", true));
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
